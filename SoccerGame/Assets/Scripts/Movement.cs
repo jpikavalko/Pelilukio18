f257fad7a8dfaf929f318d7e5cd19ca99ef13a47
Jukka Ikävalko <jpikavalko@gmail.com>
2018-02-01 16:43:24 +0200

    SoccerGame

---
+++ SoccerGame/Assets/Scripts/Movement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Movement script on ennemminkin "Class" eli "luokka". Kakoispiste ja toinen luokka perässä tarkoittaa
+//että perimme MonoBehaviour -luokasta jotakin. Jos sitä ei olisi, emme voisi käyttää suoraan
+//Start() ja Update() -funktioita.
+public class Movement : MonoBehaviour
+{
+    //Rigidbody tyypin muuttuja (tässä tapauksessa komponentti) nimeltä "rb"
+    Rigidbody rb;
+
+    //public = julkinen = näkyy inspectorissa
+    //luodaan float tyyppinen muuttuja, jonka nimi on speed ja joka alustetaan arvoon 5
+    public float speed = 5f;
+
+    //int tyypin muuttuja nimeltä playerNumber.
+    public int playerNumber;
+
+    //Start-metodia kutsutaan pelin käynnistyessä sen ensimmäisellä framella.
+    //Frame per second on ihanteellisesti 60 tai enemmän.
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //Näppäin inputit tapahtuvat aina Update-metodin (= funktion) sisällä.
+    void Update()
+    {
+        //Jos pelaajan arvo = 1, niin suoritamme kaarisulkujen sisällä olevan pätkän koodia.
+        //Jos näin ei ole, siirrymme scopen yli seuraavana kohtaan.
+        if (playerNumber == 1)
+        {
+            //GetAxis "Vertical" ja "Horizontal" löytyvät Unitysta ->
+            //Edit -> Project Settings -> Input (aukeaa Inspectoriin)
+            float v = Input.GetAxis("Vertical");
+            float h = Input.GetAxis("Horizontal");
+
+            //Kutsutaan itse luomaamme metodia MovePlayer(), jolle annetaan parametreiksi
+            //inputista saamamme float v ja float h.
+            MovePlayer(h, v);
+        }
+        if (playerNumber == 2)
+        {
+            float v = Input.GetAxis("Vertical2");
+            float h = Input.GetAxis("Horizontal2");
+            MovePlayer(h, v);
+        }
+    }
+
+    //Metodi joka ottaa parametreikseen (sulkujen sisällä) kaksi arvoa "float h" ja "float v"
+    void MovePlayer(float h, float v)
+    {
+        //tämä ei ole lainkaan pakollinen vaihe vaan...
+        float newSpeed = speed * Time.deltaTime;
+
+        //...voimme kirjoittaa seuraavan myös:
+        //transform.Translate(h * speed * Time.deltaTime, 0f, v * speed * Time.deltaTime);
+        transform.Translate(h * newSpeed, 0f, v * newSpeed);
+    }
+
+}
---
+++ SoccerGame/Assets/Scripts/Wall.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Jos koodi näyttää sekavalta, voit huoletta poistella kaikki kommentit.
+//Ne eivät vaikuta itse suorituskykyyn tai mihinkään.
+public class Wall : MonoBehaviour {
+
+    //asetamme inspectorissa, kumman maali on kyseessä.
+    public int playerGoal;
+
+    //Tämä metodi kertoo, mitä tapahtuu, kun toinen objekti astuu tämän
+    //peliobjektin colliderin rajojen sisälle.
+    private void OnTriggerEnter(Collider other)
+    {
+        //Jos other ("muu") on tägätty nimellä "Ball", niin suorita seuraavat lauseet
+        if(other.gameObject.tag == "Ball")
+        {
+            if (playerGoal == 1)
+                ResetBall(2, other);
+            if (playerGoal == 2)
+                ResetBall(1, other);
+        }
+    }
+
+    //Oma metodi nimeltä ResetBall, jolle syötämme parametriksi kokonaislukumuuttujan "goal"
+    //ja Colliderin nimeltä "other".
+    void ResetBall(int goal, Collider other)
+    {
+        //Logataan consoleen (Window -> Console) teksti. Huomaa plus-merkit ja katso, miten ne toimivat.
+        Debug.Log("Pelaaja " + goal + " teki maalin");
+
+        //resetoidaan pallon positio alkupisteeseen.
+        other.gameObject.transform.position = new Vector3(0f, 0.5f, 0f);
+
+        //Se ei kuitenkaan resetoi pallon momenttumia eli kaivamme toisen objektin (tässä tapauksessa pallo)
+        //rigidbodyn ja asetamme sieltä löytyvän velocityn arvoon "nolla"
+        other.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+        //Pelkkä liike-energian pysäyttäminen ei riitä, sillä mukana on myös kierimisliike. Asetetaan sekin nollaan.
+        other.gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
+        //Vector3.zero tarkoittaa samaa kuin "new Vector3(0f, 0f, 0f)"
+
+        //Pelaajat eivät resetoidu. Se on helppo tehdä, katsotaan se myöhemmin.
+    }
+}
