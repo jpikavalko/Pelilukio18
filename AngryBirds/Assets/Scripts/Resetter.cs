3996c3b5b8be1543cdf650077daeeba5fec1bbf9
Jukka <jpikavalko@gmail.com>
2018-02-17 09:01:20 +0200

    Angry Birds update 2

---
+++ AngryBirds/Assets/Scripts/Resetter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Resetter : MonoBehaviour {
+
+    public Rigidbody2D projectile;
+    public float resetSpeed = 0.025f;
+
+    private float resetSpeedSqr;
+    private SpringJoint2D spring;
+
+	void Start () {
+        resetSpeedSqr = resetSpeed * resetSpeed;
+        spring = projectile.GetComponent<SpringJoint2D>();
+	}
+	
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reset();
+        }
+
+        if (spring == null && projectile.velocity.sqrMagnitude < resetSpeedSqr)
+        {
+            Reset();
+        }
+
+	}
+
+    void Reset()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.GetComponent<Rigidbody2D>() == projectile)
+        {
+            Reset();
+        }
+    }
+
+}
---
+++ AngryBirds/Assets/Scripts/TargetDamage.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetDamage : MonoBehaviour {
+
+    Rigidbody2D rigidbody;
+    BoxCollider2D collider;
+    public int hitPoints = 2;
+    public Sprite damageSprite;
+    public float damageImpactSpeed;
+
+    private int currentHitPoints;
+    private float damageImpactSpeedSqr;
+    private SpriteRenderer spriteRenderer;
+
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+	void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        currentHitPoints = hitPoints;
+        damageImpactSpeedSqr = damageImpactSpeed * damageImpactSpeed;
+	}
+	
+
+	void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag != "Damager") {
+            return;
+        }
+        if (collision.relativeVelocity.sqrMagnitude < damageImpactSpeedSqr) {
+            return;
+        }
+
+        spriteRenderer.sprite = damageSprite;
+        currentHitPoints--;
+
+        if (currentHitPoints <= 0)
+        {
+            Kill();
+        }
+    }
+
+    void Kill()
+    {
+        spriteRenderer.enabled = false;
+        collider.enabled = false;
+        rigidbody.isKinematic = true;
+        //particle system if wanted
+    }
+}
