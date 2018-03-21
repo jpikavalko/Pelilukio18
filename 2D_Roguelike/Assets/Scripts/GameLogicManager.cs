587b8ec826ee8d4ad5f827338de5aa2ff95e8183
Jukka <jpikavalko@gmail.com>
2018-03-21 11:30:55 +0200

    Rogue2D-project added

---
+++ 2D_Roguelike/Assets/Scripts/GameLogicManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogicManager : MonoBehaviour {
+
+    public static GameLogicManager instance = null;
+    public BoardManager boardScript;
+
+    private int level = 3;
+
+    void Awake()
+    {
+        if (instance = null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject); //kun uusi scene alkaa se luo uuden GameManagerin. tämä tuohaisi sen.
+        }
+
+        DontDestroyOnLoad(gameObject); // tämä varmistaa, ettei gameobjectia tuhota.
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+	void InitGame() {
+        boardScript.SetupScene(level);
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ 2D_Roguelike/Assets/Scripts/Loader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour {
+
+    public GameObject gameManager;
+
+	void Awake () {
+		if (GameLogicManager.instance == null)
+        {
+            Instantiate(gameManager);
+        }
+	}
+}
